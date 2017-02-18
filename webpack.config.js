fd406d1d279610fab56702e840ac1d104dc907cd
Micah Stubbs <micah.stubbs@gmail.com>
2017-02-17 21:11:13 -0800

    add webpack config file with babel loader and es2015 preset

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {  
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
